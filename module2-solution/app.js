f3b29e68d4976221da643315999c07d653e26797
Emanuele <manufederici72@gmail.com>
2020-04-19 17:56:46 +0200

    some things

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,30 +1,52 @@
 (function () {
-    'use strict';
+'use strict';
     
-    angular.module('LunchCheck', [])
-    
-    .controller('controller', controller);
-    controller.$inject = ['$scope'];
-    
-    function controller($scope) {
-        $scope.message = "";
-        $scope.input = "";
-        $scope.parse = function() {
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-            var inputArray = $scope.input.split(',');
-            for(var i = inputArray.length - 1; i >= 0; i--) {
-                if(inputArray[i] == "" || inputArray[i] == " ") {
-                    inputArray.splice(i, 1);
-                }
-            }
-
-            if ($scope.input == "" || $scope.input == " ") {
-                $scope.message = "Please enter data first"
-            }
-            else if (inputArray.length <= 3) {$scope.message = "Enjoy!"}
-            else {$scope.message = "Too much!"}
-
-        }
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+    var toBuy = this;
+    toBuy.listToBuy = ShoppingListCheckOffService.getList1();
+    console.log(listToBuy)
+    toBuy.bought = function(itemIndex){
+        ShoppingListCheckOffService.removeItem(itemIndex)
     }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController() {
+    var Bought = this;
+    Bought.listBought = ShoppingListCheckOffService.getList2;
+}
+
+function ShoppingListCheckOffService() {
+    var service = this;
+
+    var item1 = { name: "cookies", quantity: 1 };
+    var item2 = { name: "pineapple", quantity: 2 };
+    var item3 = { name: "pizza", quantity: 3 };
+    var item4 = { name: "apple", quantity: 4 };
+    var item5 = { name: "onion", quantity: 5 };
+
+    var list1 = [item1, item2, item3, item4, item5];
+    var list2 = [];
+
+    service.addItem = function (item) {
+        list2.push(item);
+    };
+    service.removeItem = function (itemIndex) {
+        list1.splice(itemIndex, 1);
+    };
     
+    service.getList1 = function () {
+        return list1;
+    };
+    service.getList2 = function () {
+        return list2;
+    };
+}
+
 })();
