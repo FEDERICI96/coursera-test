a61a16401fe82c020a23fad7df0e8b815559a1fb
Emanuele <manufederici72@gmail.com>
2020-04-19 18:39:56 +0200

    ...

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -10,37 +10,33 @@ ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService){
     var toBuy = this;
     toBuy.listToBuy = ShoppingListCheckOffService.getList1();
-    console.log(listToBuy)
     toBuy.bought = function(itemIndex){
         ShoppingListCheckOffService.removeItem(itemIndex)
     }
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-function AlreadyBoughtController() {
-    var Bought = this;
-    Bought.listBought = ShoppingListCheckOffService.getList2;
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
+    bought.listBought = ShoppingListCheckOffService.getList2();
 }
 
 function ShoppingListCheckOffService() {
     var service = this;
 
-    var item1 = { name: "cookies", quantity: 1 };
-    var item2 = { name: "pineapple", quantity: 2 };
-    var item3 = { name: "pizza", quantity: 3 };
-    var item4 = { name: "apple", quantity: 4 };
-    var item5 = { name: "onion", quantity: 5 };
+    var item1 = { name: "cookies", quantity: 10 };
+    var item2 = { name: "pineapple", quantity: 25 };
+    var item3 = { name: "pizza", quantity: 30 };
+    var item4 = { name: "apple", quantity: 45 };
+    var item5 = { name: "onion", quantity: 50 };
 
     var list1 = [item1, item2, item3, item4, item5];
     var list2 = [];
 
-    service.addItem = function (item) {
-        list2.push(item);
-    };
     service.removeItem = function (itemIndex) {
+        list2.push(list1[itemIndex]);
         list1.splice(itemIndex, 1);
     };
-    
     service.getList1 = function () {
         return list1;
     };
